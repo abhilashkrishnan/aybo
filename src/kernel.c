5d33e5531b06e4201150e6dd28e70c62f59f79dc
osboxes.org <abhilashk.cse@gmail.com>
2023-06-08 22:12:35 +0530

    Interrupt Descriptor Table Functions

--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,45 @@
 #include "kernel.h"
+#include "idt/idt.h"
+#include <stdint.h>
+#include <stddef.h>
 
 uint16_t* video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
 uint16_t terminal_make_char(char c, char color) 
 {
     return (color << 8) | c;
+}
+
+void terminal_putchar(int x, int y, char c, char color) 
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
+}
+
+void terminal_writechar(char c, char color)
+{
+    if (c == '\n')
+    {
+        terminal_row += 1;
+        terminal_col = 0;
+        return;  
+    }
+    
+    if (c == '\t')
+    {
+        terminal_col += 4;
+        return;
+    }
+
+    terminal_putchar(terminal_col, terminal_row, c, color);
+    terminal_col += 1;
+
+    if (terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
 }
 
 void terminal_initialize() 
@@ -15,21 +50,41 @@ void terminal_initialize()
     {
         for (int x = 0; x < VGA_WIDTH; x++)
         {
-            video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(' ', 0);
+            terminal_putchar(x, y, ' ', 1);
         }
     }
     
+}
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+
+    while (str[len])
+    {
+        len++;
+    }
+
+    return len;
+}
+
+void print(const char* str)
+{
+    size_t len = strlen(str);
+
+    for (int i = 0; i < len; i++) {
+        terminal_writechar(str[i], 15);
+    }
 }
 
 void kernel_main() 
 {
     terminal_initialize();
 
-    video_mem[0] = terminal_make_char('A', 4);
-    video_mem[1] = terminal_make_char('Y', 4);
-    video_mem[2] = terminal_make_char('B', 4);
-    video_mem[3] = terminal_make_char('O', 4);
-    video_mem[4] = terminal_make_char(' ', 0);
-    video_mem[5] = terminal_make_char('O', 4);
-    video_mem[6] = terminal_make_char('S', 4);
+    print("AYBO OS\n");
+    print(" \n");
+    print("Copyright Abhilash Krishnan\n");
+
+    //Initialize Interrupt Descriptor Table
+    idt_init();
+
 }
