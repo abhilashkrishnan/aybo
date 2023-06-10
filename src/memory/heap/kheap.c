5ce9f54861eff4ac0969b43bdaeaab63dd84022b
osboxes.org <abhilashk.cse@gmail.com>
2023-06-10 15:40:20 +0530

    Kernel Heap implementation started

---
+++ src/memory/heap/kheap.c
@@ -0,0 +1,25 @@
+#include "kheap.h"
+#include "heap.h"
+#include "config.h"
+#include "kernel.h"
+
+struct heap kernel_heap;
+struct heap_table kernel_heap_table;
+
+void kheap_init() 
+{
+    int total_table_entries = AYBO_HEAP_SIZE_BYTES / AYBO_HEAP_BLOCK_SIZE;
+
+    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY*) AYBO_HEAP_TABLE_ADDRESS;
+    kernel_heap_table.total = total_table_entries;
+
+    void* end = (void*)(AYBO_HEAP_ADDRESS + AYBO_HEAP_SIZE_BYTES);
+
+    int res = heap_create(&kernel_heap, (void*)(AYBO_HEAP_ADDRESS), end, &kernel_heap_table);
+
+    if (res < 0)
+    {
+        print("Failed to create heap\n");
+    }
+
+}
